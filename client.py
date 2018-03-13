e50a0a8adbc0eaaa18de114c1b4bc1b46cdc502f
mibrahem <mmibrahem76@gmail.com>
2018-03-13 13:04:19 +0200

    added game logic to client

--- client.py
+++ client.py
@@ -91,6 +91,69 @@ print(client_socket.recv(1024).decode())
 while True:
     print("Game Stuff")
 
+
+def convert_empty_board_position(s):
+ new_s = list("123456789")
+ for i in range(0, 8):
+  if(s[i] != " "):
+   new_s[i] = s[i]
+ return "".join(new_s)
+
+
+def format_board(s):
+ if(len(s) != 9):
+  print("Error: there should be 9 symbols")
+  return ""
+
+ # Draw the grid board
+# print("|1|2|3|");
+# print("|4|5|6|");
+# print("|7|8|9|");
+ return "|" + s[0] + "|" + s[1] + "|" + s[2] + "|\n" + "|" + s[3] + "|" + s[4] + "|" + s[5] + "|\n" + "|" + s[6] + "|" + s[7] + "|" + s[8] + "|\n"
+
+
+while True:
+ board_content = client_socket.recv(9).decode()
+ print("Board Content" + format_board(board_content))
+
+ command = client_socket.recv(1).decode()
+
+ # If it's this player's turn to move
+ if(command == "Y"):
+		# Print out the current board with " " converted to the position number
+		print("Current board:\n" + format_board(convert_empty_board_position(board_content)))
+ else:
+		# Print out the current board
+		print("Current board:\n" + format_board(board_content))
+
+ if(command == "Y"):
+  while True:
+   position = int(input("please enter the postion (1~9): "))
+   if (position >= 1 and position <= 9):
+    if (board_content[position-1] != " "):
+     print("That postion is already taken. Please choose another")
+    else:
+     break
+   else:
+    print("enter a value between 1 to 9")
+  client_socket.send(str(position).encode())
+
+ elif (command == "N"):
+  print("current board:\n"+format_board(board_content))
+  print("Waiting for the other player to make a move")
+
+ elif (command == "D"):
+  print("It's a draw.")
+  break
+
+ elif (command == "W"):
+  print("You WIN!")
+  break
+
+ elif (command == "L"):
+  print("You lose.")
+  break
+
 # Shut down the socket (prevent more send/rec)
 client_socket.shutdown(socket.SHUT_RDWR)
 # Close Socket
