0918a0379020c78b8d3eae2d4f1613ccea54335e
Issam <assam.bj@gmail.com>
2018-03-13 12:07:50 +0200

    add players matching

--- server.py
+++ server.py
@@ -10,23 +10,24 @@ import logging.config
 import threading
 import string
 import random
+import time
 
 # command codenames between sever and client
 commands = {
-    'move': 'm',
+    'move': { 'cmd': 'm', 'len': '2'},
     'quit': 'q',
-    'echo': 'e',
-    'confirm': 'c',
+    'echo': { 'cmd': 'e', 'len': '2'},
+    'confirm': { 'cmd': 'c', 'len': '2'},
     'confirm_states': {
-        'id received': '1',
-        'role received': '2',
-        'match received': '3' 
+        'id_received': '1',
+        'game_info_received': '2',
     },
-    'lost': 'l',
-    'win': 'w',
-    'draw': 'd',
-    'your_turn': 'u',
-    'other_turn': 'o'
+    'lose': { 'cmd': 'l', 'len': '1'},
+    'win': { 'cmd': 'w', 'len': '1'},
+    'draw': { 'cmd': 'd', 'len': '1'},
+    'your_turn': { 'cmd': 'u', 'len': '1'},
+    'other_turn': { 'cmd': 'o', 'len': '1'},
+    'game_info': { 'cmd': 'g', 'len': '6'}
 }
 
 # setup logging file and format of log statement
@@ -80,7 +81,7 @@ class GameServer(Server):
     """Handle game start and clients management."""
 
     players_waitlist = []
-    active_players = [] # TODO: add logic to this
+    active_players = []
     players_count_history = 0
 
     def __init__(self, host, port):
@@ -88,6 +89,9 @@ class GameServer(Server):
         Server.__init__(self)
         # bind to port number as integer
         self.bind_server(host, int(port))
+        matchingThread = threading.Thread(target=self.find_opponent(), args=())
+        matchingThread.daemon = True             
+        matchingThread.start()
 
     def start(self):
         """Start the game server."""
@@ -102,10 +106,11 @@ class GameServer(Server):
             logger.info('Connected to: ' + str(addr[0]) + ':' + str(addr[1]))
             try:
                 new_player = Player(conn)
-                cThread = threading.Thread(target=self.__client_handler, args=(new_player,))
-                cThread.daemon = True             
-                cThread.start()
-                self.players_waitlist.append (new_player)
+                # cThread = threading.Thread(target=self.__client_handler, args=(new_player,))
+                # cThread.daemon = True             
+                # cThread.start()
+                self.players_waitlist.append(new_player)
+                self.active_players.append(new_player)
                 logger.info('Total connected clients number is: ' + str(GameServer.players_count_history))
             except Exception as e:
                 logger.error("Couldn't create a thread, terminating client session. Exception details: " + str(e))
@@ -119,42 +124,88 @@ class GameServer(Server):
         player.second('Server ended your session.')
         player.conn.close()
 
-    def __client_handler(self, player):
-        """Thread to handle client connection."""
-        # TODO: add clients connection logic, and players matching and game itself
-        while True:
+    # def __client_handler(self, player):
+    #     """Thread to handle client connection."""
+    #     while True:
 
-            mThread=threading.Thread(target=self.find_opponent, args=(player,))
-            mThread.daemon = True
-            mThread.start()
-            received = player.receive(2, 'move')
-            # if received is not None:
-            #     player.send('move', '2')
-            # if player.check_connection():
-            #     break
-            #create thread
-            #check if there is more then 2
-            #
+    #         mThread=threading.Thread(target=self.find_opponent, args=(player,))
+    #         mThread.daemon = True
+    #         mThread.start()
+    #         received = player.receive(2, 'move')
+    #         # if received is not None:
+    #         #     player.send('move', '2')
+    #         # if player.check_connection():
+    #         #     break
+    #         #create thread
+    #         #check if there is more then 2
+    #         #
 
 
-    def find_opponent(self, player1):
+    def find_opponent(self):
         """find an opponenet for the player"""
-        # TODO: implement this
-        if len(self.players_waitlist) >= 2:
-            for player2 in self.players_waitlist:
-                player1.match = player2
-                player2.match = player1
-                player1.role= "X"
-                player2.role = "O"
+        while len(self.players_waitlist) > 1:
+            logger.info('Matching players from waitlist')
+            player1 = self.players_waitlist[0]
+            player2 = self.players_waitlist[1]
+            player1.match = player2
+            player2.match = player1
+            # assign role in the game
+            player1.role= "X"
+            player2.role = "O"
+            # send the players game info
+            player1_send = player1.send_game_info()
+            player2_send = player2.send_game_info()
+            if player1_send and player2_send:
+                try:
+                    gameThread = threading.Thread(target=self.game_thread, args=(player1, player2))
+                    gameThread.daemon = True
+                    gameThread.start()
+                except Exception as e:
+                    logger.error('could not create game thread, exception message: ' + str(e))
+
+            else:
+                if player1_send is True and player2_send is False:
+                    GameServer.players_waitlist.append(player1)
+                    # TODO: send other player that his opponent is disconnected
+                if player2_send is True and player1_send is False:
+                    GameServer.players_waitlist.append(player2)
+                    # TODO: send other player that his opponent is disconnected
+
+        # wait for 1 second
+        time.sleep(1)
+
+    def game_thread(self, player1, player2):
+        """start a new game in seperate thread"""
+        try:
+            new_game = Game(player1, player2)
+            new_game.start()
+        except Exception as e:
+            logger.error('Game ended unexpectedly, exception message: ' + str(e))
 
 
-class PlayerPair:
-    """class to arrage pairs of matched players"""
+class Game:
+    """The game logic, moves, win and lose events handler"""
 
     def __init__(self, player1, player2):
-        """init function for PlayerPair."""
+        """initiate the game"""
+        logger.info('Matched player: ' + str(player1.id) + ' with opponenet: ' +
+            str(player1.opponenet) + ' the role of: ' + str(player1.role)) 
         self.player1 = player1
         self.player2 = player2
+        self.board = list('         ')
+
+    def start(self):
+        """start the current game"""
+         logger.info('Starting the game between: ' + str(new_game.player1.id) + ' and: ' + str(new_game.player2.id))
+        # move players
+        if(self.move(self.player1, self.player2))
+            return
+        if(self.move(self.player1, self.player2))
+            return
+
+    def move(self, player1, player2):
+        """move the players by turn"""
+        # TODO: implement move and win and lose
 
 class Player:
     """Player class to keep track of availble players and their status"""
@@ -169,6 +220,7 @@ class Player:
         self.id = self.__id_generator()
         logger.info('player with id: ' + str(self.id) + ' has joined.')
         self.role = None
+        self.opponenet = None
 
     def send(self, command, msg):
         """Send data from server to player"""
@@ -215,10 +267,39 @@ class Player:
             return True
         return False
 
+    def send_game_info(self):
+        """send the player his assigned role and matched opponenet."""
+        # send game info to player
+        counter = 0
+        msg = str(self.role + self.opponenet)
+        while True:
+            self.send(commands['game_info'], msg)
+            # tries to send for two times
+            counter += 1
+            logger.info('Sending game info to: ' + str(self.id) + ' for ' + counter + ' time')
+            if self.receive(commands['confirm']['len'], commands['confirm']) != commands['confirm_states']['game_info_received'] and counter == 2:
+                if self.check_connection() is not False:
+                    # try again to check if player is connected
+                    self.__lost_connection()
+                    # remove player from our lists
+                    GameServer.players_waitlist.remove(self)
+                    GameServer.active_players.remove(self)
+                    # return false
+                    return False
+                    # TODO: send other guy that opponnent is disconnected
+            else:
+                # sent and receive successful
+                logger.warning('Player: ' + str(self.id) + 'did not confirm receiving')
+                GameServer.players_waitlist.remove(self)
+                return True
+
     def __lost_connection(self):
         """Called when connection with player is lost"""
         logger.warning('Player: ' + str(self.id) + ' has lost connection!')
         # inform second player that his opponent has left the game
+        # remove the player from our lists
+        GameServer.players_waitlist.remove(self)
+        GameServer.active_players.remove(self)
         # TODO: self.conn.send()
 
     def __id_generator(self, size = 4, chars = string.ascii_lowercase + string.digits):
@@ -227,17 +308,9 @@ class Player:
 
 
 def main():
-    # If there are more than 2 arguments 
-    if(len(argv) >= 2):
-        # Set port number to argument 1
-        port = argv[1]
-    else:
-        # get the port number to bind to
-        port = input('Enter the port number: ')
-
     try:
         # start game server
-        server = GameServer('', port).start()
+        server = GameServer('', 8080).start()
         # close the socket connection before terminating the server
         server.close()
 
